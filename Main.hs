89d82a35a4a2dcc13a7b4ceb9d7aa3c445b9f2c5
Dave Aitken <dave.aitken@gmail.com>
2015-06-01 14:14:35 +0100

    Finished lens from getter and setter

--- Main.hs
+++ Main.hs
@@ -1,10 +1,8 @@
 {-# LANGUAGE RankNTypes #-}
 module Main where
 
-import qualified Data.Bifunctor as BF
-
-type Lens s t a b = Functor f => (a -> f b) -> s -> f t
-type Lens' s a = Functor f => (a -> f a) -> s -> f s
+type Lens s t a b = forall f. Functor f => (a -> f b) -> s -> f t
+type Lens' s a = forall f. Functor f => (a -> f a) -> s -> f s
 
 -- _1 :: Functor f => (a -> f b) -> (a, x) -> f (b, x)
 _1 :: Lens (a, x) (b, x) a b
@@ -16,26 +14,28 @@ _2 f (x, a) = ((,) x) <$> f a
 
 -- Make a lens out of a getter and a setter.
 lens :: (s -> a) -> (s -> b -> t) -> Lens s t a b
-lens get set = _
+lens get set = l where
+    l f s = (set s) <$> (f $ get s)
 
 -- Combine 2 lenses to make a lens which works on Either.
-choosing :: Lens s1 t1 a b -> Lens s2 t2 a b
-         -> Lens (Either s1 s2) (Either t1 t2) a b
-choosing l1 l2 = _
+--choosing :: Lens s1 t1 a b -> Lens s2 t2 a b
+--         -> Lens (Either s1 s2) (Either t1 t2) a b
+--choosing l1 l2 = _
+--
+---- Modify the target of a lens and return the result. (Bonus points if you
+---- do it without lambdas and defining new functions.)
+--(<%~) :: Lens s t a b -> (a -> b) -> s -> (b, t)
+--(<%~) l f s = _
+--
+---- Modify the target of a lens, but return the old value.
+--(<<%~) :: Lens s t a b -> (a -> b) -> s -> (a, t)
+--(<<%~) l f s = _
+--
+---- There's a () in every value. (No idea what this one is for, maybe it'll
+---- become clear later.)
+--united :: Lens' a ()
+--
+--united = _
 
--- Modify the target of a lens and return the result. (Bonus points if you
--- do it without lambdas and defining new functions.)
-(<%~) :: Lens s t a b -> (a -> b) -> s -> (b, t)
-(<%~) l f s = _
-
--- Modify the target of a lens, but return the old value.
-(<<%~) :: Lens s t a b -> (a -> b) -> s -> (a, t)
-(<<%~) l f s = _
-
--- There's a () in every value. (No idea what this one is for, maybe it'll
--- become clear later.)
-united :: Lens' a ()
-
-united = _
-
+main :: IO ()
 main = print "lols"
